79a721b297c0489ace2c1ef97cd3b27cb914b6db
Yerzhanov <dearhauer@gmail.com>
2023-03-26 18:50:06 +0600

    Пути для отображения входа, выхода пользователя, изменения и сброса пароля

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
-from users.views import user_login
+from users.views import user_login, index
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
+    path('', index, name='index'),
     path('login/', user_login, name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password_change/', auth_view.PasswordChangeView.as_view(
+        template_name='users/password_change_form.html'), name='password_change'),
+    path('password_change/done', auth_view.PasswordChangeDoneView.as_view(
+        template_name='users/password_change_done.html'), name='password_change_done'),
+    path('password_reset/', auth_view.PasswordResetView.as_view(
+        template_name='users/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done', auth_view.PasswordResetDoneView.as_view(
+        template_name='users/password_reset_done.html'), name='password_reset_done'),
 ]
