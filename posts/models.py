448a20be5b3fe00213ab59f52c2443f8d7197771
Yerzhanov <dearhauer@gmail.com>
2023-03-28 19:22:32 +0600

    Добавлен функционал конмментариев

--- posts/models.py
+++ posts/models.py
@@ -23,7 +23,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    body = models.TextField()
+    body = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now=True)
 
     class Meta:
