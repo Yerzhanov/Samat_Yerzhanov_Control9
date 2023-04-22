cbeb74e950115141ffcd7863a4a099776846da21
Yerzhanov <dearhauer@gmail.com>
2023-04-22 18:46:42 +0600

    Новые пути просмотра постов, удаления и обновления

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from posts.views import post_create, feed, like_post
+from posts.views import post_create, feed, like_post, PostDetailView, PostUpdateView, PostDeleteView, \
+    post_list_and_create
 
 urlpatterns = [
     path('create', post_create, name='create'),
     path('feed', feed, name='feed'),
     path('like', like_post, name='like'),
+
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_view'),
+    path('post/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'),
+
+    path('main/', post_list_and_create, name='main_page'),
 
     ]
